9bb6f429642d2ba34a0801fc7ba9638adbf75ce2
Nahom Garefo <harobina28@gmail.com>
2025-03-16 21:28:00 +0300

    Implement user registration API

---
+++ jwt-authentication/routes/auth.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const router = express.Router();
+
+
+router.post("/register", async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const newUser = new User({ username, password }); 
+      await newUser.save();
+      res.status(201).json({ message: "User registered successfully (insecurely)" });
+    } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
