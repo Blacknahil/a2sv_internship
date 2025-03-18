e90465d7feb6fcaac8331cf413615f72d1f15eea
Nahom Garefo <harobina28@gmail.com>
2025-03-18 19:32:54 +0300

    write go code for const,variables,loops

---
+++ backend-track/go-by-examples/variables/main.go
@@ -0,0 +1,22 @@
+package main 
+
+import "fmt"
+
+func main () {
+	var a = "initial"
+	fmt.Println(a)
+	var b int =21
+	fmt.Println(b)
+	
+	var c,d int = 1,2
+	fmt.Println(c,d)
+
+	var state = true
+	fmt.Println(state)
+
+	var e int 
+	fmt.Println(e)
+	
+	new_way_a := "apple"
+	fmt.Println(new_way_a)
+}
