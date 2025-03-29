09d802b00a956ad7df38f920b57b02d91ee9757c
Nahom Garefo <harobina28@gmail.com>
2025-03-29 16:35:48 +0300

    add task services boilerplate code

---
+++ backend-track/task_manager_api_auth/data/task_service.go
@@ -0,0 +1,30 @@
+// djhsjd
+package data
+
+import "go.mongodb.org/mongo-driver/mongo"
+
+type TaskServices struct {
+	TaskCollection *mongo.Collection
+}
+
+func (ts *TaskServices) CreateTask() {
+
+}
+
+func (ts *TaskServices) GetAllTasks() {
+
+}
+
+func (ts *TaskServices) GetTaskById() {
+
+}
+
+func (ts *TaskServices) UpdateTask() {
+
+}
+
+func (ts *TaskServices) DeleteTask() {
+
+}
+
+// jkjsdkjsdkj
