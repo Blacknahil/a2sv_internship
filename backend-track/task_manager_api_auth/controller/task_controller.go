31b3bc8757e0ea527217d9e2b0f894ec71b384e0
Nahom Garefo <harobina28@gmail.com>
2025-03-30 17:40:19 +0300

    implement the task controller

--- backend-track/task_manager_api_auth/controller/task_controller.go
+++ backend-track/task_manager_api_auth/controller/task_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"context"
 	"net/http"
 	"task_manager_api_auth/data"
 	"task_manager_api_auth/models"
@@ -14,82 +15,82 @@ type TaskController struct {
 
 func (tc *TaskController) CreateTask(ctx *gin.Context) {
 
-	// var newTask models.Task
+	var newTask models.Task
 
-	// if err := ctx.ShouldBindJSON(&newTask); err != nil {
+	if err := ctx.ShouldBindJSON(&newTask); err != nil {
 
-	// 	ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	// 	return
-	// }
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 	// // fmt.Println("controller", newTask)
 	// // gin.Logger(newTask)
 
-	// createdTask, err := data.TaskServices.CreateTask(context.Background(), &newTask)
-	// if err != nil {
-	// 	ctx.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
-	// 	return
-	// }
+	createdTask, err := tc.TaskServices.CreateTask(context.Background(), &newTask)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
 
-	ctx.JSON(http.StatusCreated, gin.H{"message": "success create task"})
+	ctx.JSON(http.StatusCreated, gin.H{"message": "Successfully created task", "task": createdTask})
 }
 
 func (tc *TaskController) GetAllTasks(ctx *gin.Context) {
-	// tasks, err := tc.Services.GetAllTasks(context.Background())
+	tasks, err := tc.TaskServices.GetAllTasks(context.Background())
 
-	// if err != nil {
-	// 	ctx.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
-	// 	return
-	// }
-	ctx.JSON(http.StatusOK, gin.H{"tasks": []models.Task{}})
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"tasks": tasks})
 
 }
 
 func (tc *TaskController) GetTasksById(ctx *gin.Context) {
 
-	// id := ctx.Param("id")
-	// task, err := tc.Services.GetTaskById(context.Background(), id)
+	id := ctx.Param("id")
+	task, err := tc.TaskServices.GetTaskById(context.Background(), id)
 
-	// if err != nil {
-	// 	ctx.JSON(http.StatusNotFound, err.Error())
-	// 	return
-	// }
-	ctx.JSON(http.StatusOK, models.Task{})
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, err.Error())
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"task": task})
 }
 
 func (tc *TaskController) UpdateTask(ctx *gin.Context) {
 
-	// id := ctx.Param("id")
+	id := ctx.Param("id")
 
-	// var updatedTask models.Task
+	var updatedTask models.Task
 
-	// if err := ctx.ShouldBindJSON(&updatedTask); err != nil {
+	if err := ctx.ShouldBindJSON(&updatedTask); err != nil {
 
-	// 	ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	// 	return
-	// }
-	// err := tc.Services.UpdateTask(context.Background(), id, updatedTask)
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	err := tc.TaskServices.UpdateTask(context.Background(), id, updatedTask)
 
-	// if err != nil {
-	// 	ctx.JSON(http.StatusNotFound, gin.H{"message": err.Error()})
-	// 	return
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"message": err.Error()})
+		return
 
-	// }
+	}
 
 	ctx.JSON(http.StatusOK, gin.H{"message": "Task Updated successfully"})
 }
 
 func (tc *TaskController) DeleteTask(ctx *gin.Context) {
-	// id := ctx.Param("id") // Get the task ID from the URL parameter
+	id := ctx.Param("id") // Get the task ID from the URL parameter
 
-	// err := tc.Services.DeleteTask(context.Background(), id)
-	// if err != nil {
-	// 	if err.Error() == "task not found" {
-	// 		ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
-	// 	} else {
-	// 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	// 	}
-	// 	return
-	// }
+	err := tc.TaskServices.DeleteTask(context.Background(), id)
+	if err != nil {
+		if err.Error() == "task not found" {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		return
+	}
 
 	ctx.JSON(http.StatusOK, gin.H{"message": "Task deleted successfully"})
 
