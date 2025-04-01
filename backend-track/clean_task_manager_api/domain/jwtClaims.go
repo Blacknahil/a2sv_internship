ad882f93d1f1b8fd7a67659d1ede531bd26a88b5
Nahom Garefo <harobina28@gmail.com>
2025-04-02 00:31:44 +0300

    define jwt custom claims for token generation and validation

---
+++ backend-track/clean_task_manager_api/domain/jwtClaims.go
@@ -0,0 +1,13 @@
+package domain
+
+import (
+	"github.com/golang-jwt/jwt"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type CustomJWTClaims struct {
+	UserID primitive.ObjectID `bson:"user_id,omitempty" json:"user_id"`
+	Email  string             `json:"email"`
+	Role   string             `json:"role"`
+	jwt.StandardClaims
+}
